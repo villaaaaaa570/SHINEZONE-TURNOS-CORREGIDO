c106063534204924025e5c3900dae9fd6dd85041
villaaaaaa570 <tomascraftin@gmail.com>
2026-09-09 11:41:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 const CACHE='shinezone-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-1024.png']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
