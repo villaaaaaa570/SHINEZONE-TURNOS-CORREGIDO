cf1c2e4aa2c22c8a7cffb2c0d6b3d3fa1232d1b2
villaaaaaa570 <tomascraftin@gmail.com>
2026-09-09 14:35:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE='shinezone-v6';
-const ASSETS=['./','./index.html','./manifest.json','./icon-1024.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='shinezone-v6').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const CACHE='shinezone-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-1024.png']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
